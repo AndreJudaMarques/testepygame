cb0cde1f799d1801fa20a4f163bd5bedee10c327
André Judá <judatrainer@gmail.com>
2023-03-15 14:25:01 -0300

    colisao aleatoria colidindo e mudando de posicao aleatoriamente

--- teste.py
+++ teste.py
@@ -1,26 +1,32 @@
 
-import pygame
+import pygame #importa codigos do jogo
 
-from pygame.locals import *
+from pygame.locals import * #importa tudo dess classe
 
-from sys import exit
+from sys import exit #inclui o x pra fechar a janela
 
-pygame.init()
+from random import randint #funcao sorteia valores ()
+
+pygame.init() #inicia o jogo
 
 
 larguraTela = 640
 alturaTela = 480
-tela = pygame.display.set_mode((larguraTela, alturaTela))
-pygame.display.set_caption('JogoPyTest')
-
-relogio = pygame.time.Clock()
 
 x = larguraTela / 2 - 20 #20 (referencia da largura do objeto)
 y = alturaTela / 2
 
+xAzul = randint(40,600) 
+yAzul = randint(50, 430)
+
+tela = pygame.display.set_mode((larguraTela, alturaTela))
+pygame.display.set_caption('JogoPyTest')
+
+relogio = pygame.time.Clock() #coloca elemento timer no jogo
+
 while True:
-    relogio.tick(40)
-    tela.fill((0, 0, 0))
+    relogio.tick(40) #controla os frames do elemento time
+    tela.fill((128, 128, 128))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -47,6 +53,11 @@ while True:
     if pygame.key.get_pressed()[K_DOWN]:
         y += 20
 
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 40,40))
-       
+    retVermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40,40))
+    retAzul = pygame.draw.rect(tela, (0, 0, 255), (xAzul, yAzul, 40,40))
+
+    if retVermelho.colliderect(retAzul):
+        xAzul = randint(40,600)
+        yAzul = randint(50, 430)
+
     pygame.display.update()
